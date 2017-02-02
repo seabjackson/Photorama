a011ef775395f3ce125feb581e79574dcd448855
Seab <honeyboyseab@gmail.com>
2017-02-02 13:54:58 -0500

    initial set up of storyboard and navigation controller

---
+++ FlickrPhotos/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  FlickrPhotos
+//
+//  Created by Seab on 2/2/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
