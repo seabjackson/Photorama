00e2151a8441c55df5f4136117bb4a8fe1c2c2d9
Seab <honeyboyseab@gmail.com>
2017-02-02 16:19:45 -0500

    constructed the url

---
+++ FlickrPhotos/FlickrPhotos/FlickrAPI.swift
@@ -0,0 +1,121 @@
+//
+//  FlickrAPI.swift
+//  FlickrPhotos
+//
+//  Created by Seab on 2/2/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+    private static let baseURLString = "https://api.flickr.com/services.rest"
+    private static let apiKey = "7d70893aeadad608a34e0780949e1c10"
+    
+    static var interestingPhotosURL: URL {
+        return flickrURL(method: .interestingPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+    
+    private static func flickrURL(method: Method, parameters: [String: String]?) -> URL {
+        var components = URLComponents(string: baseURLString)!
+        
+        var queryItems = [URLQueryItem]()
+        
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": apiKey,
+        ]
+        
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        return components.url!
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
