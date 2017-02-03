d35f566a425bf0c7c781c86e106437414ef385ea
Seab <honeyboyseab@gmail.com>
2017-02-03 01:46:34 -0500

    added functions to parse the json data

--- FlickrPhotos/FlickrPhotos/FlickrAPI.swift
+++ FlickrPhotos/FlickrPhotos/FlickrAPI.swift
@@ -12,6 +12,10 @@ enum Method: String {
     case interestingPhotos = "flickr.interestingness.getList"
 }
 
+enum FlickrError: Error {
+    case invalidJSONData
+}
+
 struct FlickrAPI {
     private static let baseURLString = "https://api.flickr.com/services/rest"
     private static let apiKey = "7d70893aeadad608a34e0780949e1c10"
@@ -30,7 +34,7 @@ struct FlickrAPI {
             "format": "json",
             "nojsoncallback": "1",
             "api_key": apiKey,
-        ]
+            ]
         
         for (key, value) in baseParams {
             let item = URLQueryItem(name: key, value: value)
@@ -46,6 +50,24 @@ struct FlickrAPI {
         components.queryItems = queryItems
         print(components.url)
         return components.url!
+    }
+    
+    private static func photos(fromJSON data: Data) -> PhotosResult {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+            
+            guard let jsonDictionary = jsonObject as? [AnyHashable: Any],
+                let photos = jsonDictionary["photos"] as? [String: Any],
+                let photosArray = photos["photo"] as? [[String: Any]] else {
+                    // the json structure doesn't match our expectations
+                    return .failure(FlickrError.invalidJSONData)
+            }
+            
+            var finalPhotos = [Photo]()
+            return .success(finalPhotos)
+        } catch let error {
+            return .failure(error)
+        }
     }
 }
 
--- FlickrPhotos/FlickrPhotos/PhotoStore.swift
+++ FlickrPhotos/FlickrPhotos/PhotoStore.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+enum PhotosResult {
+    case success([Photo])
+    case failure(Error)
+}
+
 class PhotoStore {
     private let session: URLSession = {
        let config = URLSessionConfiguration.default
@@ -19,8 +24,11 @@ class PhotoStore {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) in
             if let jsonData = data {
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    print(jsonString)
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                    print(jsonObject)
+                } catch let error {
+                    print("Error creating JSON object: \(error)")
                 }
             } else if let requestError = error {
                 print("Error fetching interesting photos: \(requestError)")
