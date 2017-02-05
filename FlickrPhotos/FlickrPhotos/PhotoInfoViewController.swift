fa5863061dded259f26f1037a8ec8d4c1c069e9e
Seab <honeyboyseab@gmail.com>
2017-02-04 23:31:38 -0500

    added a new view controller that displays the selected photo when it is selected from the grid layout of the collection view

--- FlickrPhotos/FlickrPhotos/PhotoInfoViewController.swift
+++ FlickrPhotos/FlickrPhotos/PhotoInfoViewController.swift
@@ -6,4 +6,92 @@
 //  Copyright © 2017 Seab Jackson. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { (result) in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
