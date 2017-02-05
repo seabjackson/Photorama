50ccbe7b60184e0d1b4f49d3ca8ab6dd3cee0ba3
Seab <honeyboyseab@gmail.com>
2017-02-04 20:56:48 -0500

    customized a collection view cell and added a helper method for an activity indicator or spinner to spin whenever there aren't any images in the cell yet

---
+++ FlickrPhotos/FlickrPhotos/PhotoCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoCollectionViewCell.swift
+//  FlickrPhotos
+//
+//  Created by Seab on 2/4/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+}
